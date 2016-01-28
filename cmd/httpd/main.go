a8e6684aeb2119fe0625d4477e1435aa960fe41e
Takashi Toyoshima <toyoshim@gmail.com>
2016-01-28 16:10:22 +0000

    Add logging

--- cmd/httpd/main.go
+++ cmd/httpd/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/toyoshim/gomolog"
 )
 
 func main() {
-  fmt.Println("listening on " + os.Getenv("PORT") + " ...")
-  err := http.ListenAndServe(":" + os.Getenv("PORT"), http.FileServer(http.Dir("static")))
-  if err != nil {
-    panic(err)
-  }
+	log := gomolog.Open(os.Getenv("MONGOLAB_URI"), "log")
+	defer log.Close()
+
+	fmt.Println("listening on " + os.Getenv("PORT") + " ...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), http.FileServer(http.Dir("static")), log.Logger())
+	if err != nil {
+		panic(err)
+	}
 }
